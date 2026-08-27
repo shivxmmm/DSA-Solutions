6d82c3bec7a574d08a4a224868314d20b1269f12
Shivam Kakade <167352358+shivxmmm@users.noreply.github.com>
2026-08-27 19:31:47 +0530

    Time: 0 ms (100%), Space: 43.5 MB (59.56%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -2,12 +2,26 @@ class Solution {
     public int hIndex(int[] citations) {
 
         int n = citations.length;
-        Arrays.sort(citations);
+        int[] bucket = new int[n+1];
 
         for(int i = 0; i < n; i++){
 
-            if(citations[i] >= n - i)
-            return n - i;
+            int x = citations[i];
+
+            if(x >= n){
+                bucket[n]++;
+            }else{
+                bucket[x]++;
+            }
+        }
+        
+        int count = 0;
+        for(int i = n; i >= 0; i--){
+
+            count += bucket[i];
+            if(count >= i){
+                return i;
+            }
         }
         return 0;
     }
