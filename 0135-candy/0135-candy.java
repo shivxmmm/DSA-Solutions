f54ca4ee1a6729819c464cef472cf0e11a7f1986
Shivam Kakade <167352358+shivxmmm@users.noreply.github.com>
2026-08-29 19:01:52 +0530

    Time: 3 ms (86.59%), Space: 52.4 MB (11.74%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int candy(int[] ratings) {
+        
+        int n = ratings.length;
+        int[] candi = new int[n];
+
+        Arrays.fill(candi, 1);
+
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                candi[i] = candi[i - 1] + 1;
+            }
+        }
+
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1] && candi[i + 1] >= candi[i]){
+                candi[i] = candi[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for(int candy : candi){
+            ans += candy;
+        }
+        return ans;
+    }
+}
